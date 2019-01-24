8cbf0af76293ce005d4e13863a8fec04bfcf2528
Peter Sigurdson <peter@petersigurdson.net>
2019-01-24 16:25:26 -0500

    4:30 Code Commit

--- C098765/Program.cs
+++ C098765/Program.cs
@@ -6,10 +6,68 @@ using System.Threading.Tasks;
 
 namespace C098765
 {
+    public class Customer
+    {
+        public int ID { get; set; }
+        public string Name { get; set;  }
+
+        public void Promote() {
+            var rating = CalculateRating();
+            if (rating == 0)
+            {
+                Console.WriteLine("Promoted to Level 1");
+            }
+            else { Console.WriteLine("Promoted to Level 2");}
+        }
+
+        public int CalculateRating()
+        {
+            return 0;
+        }
+
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            var customer = new Customer();
+
+            //var dog_and_cat = new Car();
+            //var bowling = new Car();
+            //Console.WriteLine("how many cars: {0}", Vehicle.howManyCars);
+
+            //Book myBook = new Book();
+            //myBook.SetTitle("A Separate Peace");
+            //Console.WriteLine(myBook.GetTitle());
+
         }
+    }
+
+    class Vehicle
+    {
+        public static int howManyCars=0;
+        public String Color = "Blue";
+    }
+
+    class Car : Vehicle
+    {   
+        public String Color = "Red";
+        public Car()
+        {   
+            Vehicle.howManyCars++;
+            Console.WriteLine("my color is " + Color);
+        }
+    }
+
+    class BookCollection
+    {
+
+    }
+    class Book : BookCollection
+    {
+        private String Title;
+        public void SetTitle(String aTitle) { this.Title = aTitle;  }
+         this.Title; }
     }
 }
